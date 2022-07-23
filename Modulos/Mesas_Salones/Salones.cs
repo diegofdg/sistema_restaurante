bd4b6a9d1ce93a0006efb247d225c1ffcd41038f
diegofdg <diegofdg@gmail.com>
2022-07-23 18:05:18 -0300

    diseño de salones

---
+++ Modulos/Mesas_Salones/Salones.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SistemaRestaurante.Modulos.Mesas_Salones
+{
+    public partial class Salones : Form
+    {
+        public Salones()
+        {
+            InitializeComponent();
+        }
+        private void Salones_Load(object sender, EventArgs e)
+        {
+            FormBorderStyle = FormBorderStyle.None;
+        }
+    }
+}
